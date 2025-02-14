f23e6e00c637bb4c2807c0c4cdee9631efda905f
Harry J.E Day <harry@nurturecloud.com>
2025-02-14 15:31:01 +1100

    ARC-6669 reduce error level of log - we expect this to happen are redis will have timeouts, etc
    This was actually previously done in ARC-3625 but only on a forked branch

--- src/main/java/com/googlecode/objectify/cache/MemcacheServiceRetryProxy.java
+++ src/main/java/com/googlecode/objectify/cache/MemcacheServiceRetryProxy.java
@@ -54,9 +54,9 @@ public class MemcacheServiceRetryProxy implements InvocationHandler
 				return meth.invoke(this.raw, args);
 			} catch (InvocationTargetException ex) {
 				if (i == (this.tries - 1))
-					log.error("Memcache operation failed, giving up", ex);
+					log.warn("Memcache operation failed, giving up on the cache", ex);
 				else
-					log.warn("Error performing memcache operation, retrying: " + meth, ex);
+                    log.warn("Error performing memcache operation, retrying: {}", meth, ex);
 			}
 		}
 		
